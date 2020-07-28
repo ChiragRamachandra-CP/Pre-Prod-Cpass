6ac909f4cad347555735a5581684ca837495038b
suraj pandey <surajsoft20@gmail.com>
2020-07-28 22:53:46 +0530

    SAT Landing

--- src/components/slider/SliderLandingPage.js
+++ src/components/slider/SliderLandingPage.js
@@ -14,16 +14,16 @@ const SliderLandingPage = () => {
 							</div>
 						</div>
 						<img
-							src={'../../assets/images/Score1500SAT.png'}
+							src={'../../assets/images/SAT.png'}
 							alt="Banner" className="satbnner"
 						/>
 						<div className="container top-bnnr">
 							<div className="row">
-								{/*<div className="col-md-8 banner-cont">
-									<h1>Join Your Dream College.<br/><span className="bnnrhedsndlne">Learn from Global College Admission Advisors</span></h1>
-									<h5>100+ Live Online Sessions Annually Video Lessons</h5>
-									<p className="bannerbtnbl" data-toggle="modal" data-target="#myModal">Create Your Free Account</p>
-</div>*/}
+								<div className="col-md-8 banner-cont">
+									<h1>Score 1500+ With SAT Masterclass<span className="bnnrhedsndlne"><br/>COLLEGEPASS STUDENT HAVE CONSISTENTLY SCORED 1500+ ON CONSECUTIVE SAT TESTS IN 2019-20.</span></h1>
+									<h5>LEARN FROM TOP RATED, INTERNATIONAL TUTORS!</h5>
+									<p className="bannerbtnbl">Book Your Free Consultant</p>
+</div>
 								
 							</div>
 						</div>
--- src/pages/landingPages/IBTutoring.js
+++ src/pages/landingPages/IBTutoring.js
@@ -1,16 +1,41 @@
 import React, { Fragment } from 'react';
 import AfterLoggedInHeader from '../../components/header/AfterLoggedInHeader';
 import Footer from '../../components/footer/Footer';
+import SliderLandingPage from '../../components/slider/SliderLandingPage';
+import LandingPageIcon from '../../components/marketBanner/LandingPageIcon';
+//import Video from '../../components/video/Video';
+import Testimonial from '../../components/testimonial/Testimonial';
+import LandingPageForm from '../../components/form/LandingPageForm';
 
 const IBTutoring = () => {
 	
 	return (
 		<Fragment>
-			<AfterLoggedInHeader />
-			<div className="site-wrap page-id-7">
-				<h1 style={{textAlign: 'center', padding: '60px 0px', fontSize: '30px', fontWeight: '600'}}>Coming Soon</h1>
+			<div className="satID-2">
+				<AfterLoggedInHeader />
+				<SliderLandingPage />
+				<section className="mainSAT">
+					<div className="container">
+						<section className="single_video">
+							<div className="upcmngls">
+								<div className="frsttxt">
+									<p className="heddesc">The CollegePass Experience</p>
+									<h2 className="new-class-heading" style={{textAlign: 'left'}}>Join Your Dream College</h2>
+									<li className="fvlnetxt">CollegePass IB Tutoring is committed to helping students achieve their target IB Score.</li>
+									<li className="fvlnetxt">CollegePass Tutors are experienced and have taught at leading IB Schools.</li>
+									<li className="fvlnetxt">CollegePass Tutors have graduated from leading universities worldwide.</li>
+									<li className="fvlnetxt">CollegePass Students have Top Scored in 2019-20 with 40-42.</li>
+									<li className="fvlnetxt">CollegePass Students have a Personal Student Success Manager.</li>
+								</div>
+							</div>
+						</section>
+					</div>
+				</section>
+				<LandingPageIcon />
+				<Testimonial />
+				<LandingPageForm />
+				<Footer />
 			</div>
-			<Footer />
 		</Fragment>
 	);
 };
